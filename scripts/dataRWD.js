6c3b86735e51b3d3ce4d944b6aa672f8ea54dc67
Ryan Els <39189847+RyanGuitar@users.noreply.github.com>
2023-08-29 09:29:23 +0200

    add dataRWD

---
+++ scripts/dataRWD.js
@@ -0,0 +1,52 @@
+import {
+  getValue,
+  getId,
+} from "../helper.js"
+import {
+  whatsappStatus
+} from "./dropBox.js"
+
+function deleteAlarm() {
+  let updatedData = getValue("writingPad");
+  const alarmInput = getId("alarmInput")
+  const alarmInputValue = alarmInput.value;
+  if (!updatedData || !alarmInputValue) return;
+  getId("whatsapp").dataset.saved = "true"
+  whatsappStatus()
+  updatedData = JSON.parse(updatedData);
+  const index = updatedData.alarms.findIndex((obj) => obj.alarm === alarmInputValue);
+  if (index !== -1) {
+    if (updatedData.alarms[index].count > 1) {
+      updatedData.alarms[index].count--;
+    } else {
+      updatedData.alarms.splice(index, 1);
+    }
+    globalThis.workerX.postMessage({ msg: "write", data: JSON.stringify(updatedData)});
+  }
+}
+
+function getData() {
+  let updatedData = getValue("writingPad");
+  updatedData = updatedData ? JSON.parse(updatedData) : {"alarms": []};
+  globalThis.inputAlarm = alarmInput.value;
+  const inputAlarm = globalThis.inputAlarm;
+  if (!inputAlarm) return;
+  getId("whatsapp").dataset.saved = "false"
+  whatsappStatus()
+
+  if (!updatedData.alarms) {
+    updatedData.alarms = [];
+  }
+  const existingAlarm = updatedData.alarms.find(({ alarm }) => alarm === inputAlarm);
+  if (existingAlarm) {
+    existingAlarm.count = (existingAlarm.count || 1) + 1;
+  } else {
+    updatedData.alarms.push({ alarm: inputAlarm, count: 1 });
+  }
+  globalThis.workerX.postMessage({ msg: "merge", data: updatedData });
+}
+
+export {
+  deleteAlarm,
+  getData
+}
